ead0d119f1ba40791f888e161f056b4ae8912e1d
Brian <wamwangimathenge@gmail.com>
2023-02-15 20:18:33 +0300

    3/6 Passed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
 
-  attr_accessor :side1, :side2, :side3
+  attr_reader :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
     @side1 = side1
@@ -16,6 +16,30 @@ class Triangle
     else
       :scalene
     end
+  end
+
+  def triangle_sides
+    if side1 > 0 && side2 > 0 && side3 > 0
+      true
+    else
+      false
+    end
+  end
+
+  def triangle_inequality
+    sum1 = [side1, side2].sum
+    sum2 = [side2, side3].sum
+    sum3 = [side3, side1].sum
+
+    if sum1 > side3 && sum2 > side1 && sum3 > side2
+      true
+    else
+      false
+    end 
+  end
+
+  def validate_triangle
+    raise TriangleError unless triangle_inequality == true && triangle_sides == true
   end
 
   class TriangleError < StandardError
